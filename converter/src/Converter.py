337ff78da1f15780f10a2829682a4205cbb9e1d5
esalama01 <mehdisalama61@gmail.com>
2026-05-11 14:54:59 +0100

    created sub

--- converter/src/Converter.py
+++ converter/src/Converter.py
@@ -10,6 +10,7 @@
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 import json
+import re
 import pandas as pd
 from socceraction.data.opta.parsers import WhoScoredParser
 from socceraction.spadl.opta import convert_to_actions
@@ -47,8 +48,10 @@ keys = ["Serie-a",
 "Ligue-1",
 "Euro"]
 values = whoscored_leagues
+
 map = dict(zip(keys, values))
 
+"""
 def create_subd(name):
     path = Path(f"../data/{map[name]}")
     # Create the directories
@@ -73,14 +76,22 @@ for repo in repos:
     for n_repo in new_repos:
         sub_repos.add(n_repo)
 
-def convert(path): #takes a folder as input and converts every file in it to spadl.
+def convert(file_path): #takes a folder as input and converts every file in it to spadl.
     paths = [] 
     with os.scandir(path) as entries:
         for entry in entries:
             if entry.is_file():
                 path.append(entry.path)
-    
-        
+"""
+def create_subdirs(name, corr_year): #returns the name of thhe created subdirectory, and year of the next sub to fill it in the whoscored parser.(input like : Epl/england-premier-league-2025-2026)
+    pattern = r"\d{4}"
+    years = re.findall(pattern, corr_year)
+    year = "-".join(years)
+    path = Path(f"../data/{map[name]}/{year}")
+    # Create the directories
+    path.mkdir(parents=True, exist_ok=True)
+    return map[name], year
+
 def main():
     for repo in repos:
         print(repo)
