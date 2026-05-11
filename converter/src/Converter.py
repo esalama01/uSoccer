8a16f06f87adf8fd208167708972f67c4db51382
esalama01 <mehdisalama61@gmail.com>
2026-05-11 14:18:08 +0100

    created subd fucntion

--- converter/src/Converter.py
+++ converter/src/Converter.py
@@ -9,20 +9,85 @@
 """
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+import json
+import pandas as pd
+from socceraction.data.opta.parsers import WhoScoredParser
+from socceraction.spadl.opta import convert_to_actions
+from socceraction.data.opta.loader import _eventtypesdf
 import os
-
+from pathlib import Path
 #Get all the subtrees at {converter/data}
 
-repos = set()
+initial_dir = r"../../scraper/data"
 
-for root, dirs, files in os.walk('../../scraper/data'):
-    repos.add(root)
+whoscored_leagues = [
+    "ITA-Serie A",
+    "ENG-Premier League",
+    "INT-World Cup",
+    "GER-Bundesliga",
+    "POR-Liga Portugal",
+    "NED-Eredivisie",
+    "AMER-Copa America",
+    "ESP-La Liga",
+    "AFR-Africa Cup of Nations",
+    "EUR-Champions League",
+    "FRA-Ligue 1",
+    "EUR-Euro"
+]
+keys = ["Serie-a",
+"Epl",
+"World Cup",
+"Bundesliga",
+"Liga Portugal",
+"Netherlands Eredivisie",
+"Copa America",
+"La Liga,"
+"Afcon",
+"Ucl",
+"Ligue-1",
+"Euro"]
+values = whoscored_leagues
+map = dict(zip(keys, values))
+
+def create_subd(name):
+    path = Path(f"../data/{map[name]}")
+    # Create the directories
+    path.mkdir(parents=True, exist_ok=True)
 
 
+def scan_dir(path):
+    repos = set()
+    with os.scandir(path) as entries:
+        for entry in entries:
+            if entry.is_dir():
+                repos.add(entry.name) #--> Outputs a list of all sub_repos in my path
+    for repo in repos:
+
+
+repos = scan_dir(initial_dir)
+sub_repos = set()
+
+for repo in repos:
+    path = fr"../../scraper/data/{repo}"
+    new_repos = scan_dir(path)
+    for n_repo in new_repos:
+        sub_repos.add(n_repo)
+
+def convert(path): #takes a folder as input and converts every file in it to spadl.
+    paths = [] 
+    with os.scandir(path) as entries:
+        for entry in entries:
+            if entry.is_file():
+                path.append(entry.path)
+    
+        
 def main():
     for repo in repos:
         print(repo)
+    print("---------------------------------------------------------------")
+    for repo in sub_repos:
+        print(repo)
+
 
 if __name__ == "__main__":
     main()
